7d29a8c46c6282c74945855feb29af477248095f
benji3106 <benjamin3106@live.fr>
2026-08-03 14:00:18 +0200

    feat: add daisyui dependency and integrate theme toggle in App component

--- front_end-frameworks/react/src/App.jsx
+++ front_end-frameworks/react/src/App.jsx
@@ -1,10 +1,20 @@
-import { Camera } from "lucide-react";
+import { Camera, Moon, Sun } from "lucide-react";
 
 function App() {
   return (
     <>
      <h1 className="text-4xl text-red-500">Mon super projet React !!!</h1>
      <Camera color="red" size={48} />
+     <label className="swap swap-rotate">
+  {/* this hidden checkbox controls the state */}
+  <input className="theme-controller" type="checkbox" value="dark" />
+
+  {/* Sun icon */}
+<Sun className="swap-on text-red-500" />
+
+  {/* Moon icon */}
+<Moon className="swap-off text-red-500" />
+</label>
     </>
   );
 }
