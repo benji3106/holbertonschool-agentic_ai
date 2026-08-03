b33b7223d1c2454cf059980859606d464b8894ae
benji3106 <benjamin3106@live.fr>
2026-08-03 13:55:45 +0200

    feat: add lucide-react dependency and integrate Camera component in App

--- front_end-frameworks/react/src/App.jsx
+++ front_end-frameworks/react/src/App.jsx
@@ -1,7 +1,10 @@
+import { Camera } from "lucide-react";
+
 function App() {
   return (
     <>
      <h1 className="text-4xl text-red-500">Mon super projet React !!!</h1>
+     <Camera color="red" size={48} />
     </>
   );
 }
