1067a99365d143ec11010f415f56ad166cb082b0
benji3106 <benjamin3106@live.fr>
2026-08-04 09:57:21 +0200

    feat: add Hero component with background effects and stats display

--- front_end-frameworks/react/src/App.jsx
+++ front_end-frameworks/react/src/App.jsx
@@ -1,26 +1,13 @@
-import { Camera, Moon, Sun } from "lucide-react";
 import Header from './components/Header';
+import Hero from './sections/Hero';
 
 function App() {
   return (
     <>
-     <Header />
-     <main className="pt-20">
-      
-     </main>
-     <h1 className="text-4xl text-red-500">Mon super projet React !!!</h1>
-     <h2 className="text-2xl text-red-500">Avec Tailwind CSS et DaisyUI</h2>
-     <Camera color="red" size={48} />
-     <label className="swap swap-rotate">
-  {/* this hidden checkbox controls the state */}
-  <input className="theme-controller" type="checkbox" value="dark" />
-
-  {/* Sun icon */}
-<Sun className="swap-on text-red-500" />
-
-  {/* Moon icon */}
-<Moon className="swap-off text-red-500" />
-</label>
+      <Header />
+      <main>
+        <Hero />
+      </main>
     </>
   );
 }
